83a9ff616d6efba157c1261cce7cd515055d8596
Charlie Harding <xsanda@me.com>
2017-08-08 11:45:57 +0100

    Remove VoidBlocks

--- src/BlockViewer.purs
+++ src/BlockViewer.purs
@@ -3,6 +3,7 @@ module BlockViewer where
 import Prelude
 import Structures
 import Data.Maybe (Maybe(..), isJust, isNothing, fromMaybe)
+import Data.Monoid (class Monoid, mempty)
 import Data.Tuple (Tuple(..))
 import Data.Foldable (foldr, all)
 import Data.Array (concatMap, zip, filter, null)
@@ -15,7 +16,7 @@ import Halogen.HTML.Core as HCore
 
 type State = Maybe Block'
 
-data BlockView = BlockView Block' | InputBlock Type | VoidBlock
+data BlockView = BlockView Block' | InputBlock Type
 
 data Query a
   = IsEmpty (Boolean -> a)
@@ -48,28 +49,24 @@ myBlockViewer =
   structureRendering (Just block'@(Block' (Block name fn output inputs) connected)) =
     [ HH.div [classes ["structureView"]] [ structureToDiv (BlockView block') ]
     ]
-  --   [ HH.p_ [ HH.text (name <> " " <> foldr (<>) "" (map (\input -> show input <> " -> ") inputs) <> show output) ]
-  --   , HH.h2_ [ HH.text ("width = " <> show (getHeight $ Just block')) ]
-  --   , HH.ol_ $ map (HH.li_ <<< structureRendering) connected
-  --   ]
   structureRendering Nothing =
     [ HH.p_ [ HH.text ("Nothing") ]
     ]
   
   structureToDiv :: BlockView -> H.ComponentHTML Query
-  structureToDiv VoidBlock = HH.div [classes ["void"]] []
   structureToDiv (InputBlock typ) = HH.div [classes ["input", toClass typ]] []
   structureToDiv blockView@(BlockView (Block' (Block name fn out ins) connected)) =
     HH.div [ HP.draggable true, classes ["block", toClass out, "void" `if_` null connected] ]
-      [ HH.div [classes ["block-text"]]
-        [ HH.text (name <> " \x2237 " <> foldr (<>) "" (map (\input -> show input <> " \x2192 ") ins) <> show out) ]
-      , HH.div [classes ["block-inputs"]] $ map structureToDiv (getConnected blockView)
-      ]
+      (
+        [ HH.div [classes ["block-text"]]
+          [ HH.text (name <> " \x2237 " <> foldr (<>) "" (map (\input -> show input <> " \x2192 ") ins) <> show out) ]
+        ]
+        <> [ HH.div [classes ["block-inputs"]] $ map structureToDiv (getConnected blockView)] `if_` not (null connected)
+      )
   
   getConnected :: BlockView -> Array BlockView
-  getConnected VoidBlock = [VoidBlock]
-  getConnected (InputBlock _) = [VoidBlock]
-  getConnected (BlockView (Block' _ [])) = [VoidBlock]
+  getConnected (InputBlock _) = []
+  getConnected (BlockView (Block' _ [])) = []
   getConnected (BlockView (Block' (Block _ _ _ ins) connected)) = map maybeToBlockView $ zip ins connected
   
   maybeToBlockView :: (Tuple Type (Maybe Block')) -> BlockView
@@ -83,8 +80,8 @@ myBlockViewer =
   toClass IntType = "int"
   toClass ColourType = "colour"
   
-  if_ :: String -> Boolean -> String
-  if_ x bool = if bool then x else ""
+  if_ :: forall a . Monoid a => a -> Boolean -> a
+  if_ x bool = if bool then x else mempty
 
   eval :: Query ~> H.ComponentDSL State Query Message m
   eval = case _ of
